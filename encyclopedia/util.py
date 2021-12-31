f719dfcbd7b87c040941eb532c22a56e79e9be3d
Rajat Rajdeep <rajatrajdeep@outlook.com>
2021-12-31 15:08:00 +0530

    Entry, Index and Random Page feature is implemented.

--- encyclopedia/util.py
+++ encyclopedia/util.py
@@ -1,4 +1,5 @@
 import re
+import random
 
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
@@ -35,3 +36,10 @@ def get_entry(title):
         return f.read().decode("utf-8")
     except FileNotFoundError:
         return None
+
+
+def get_random_entry():
+    """
+    Choice and returns a random list encyclopedia entry.
+    """
+    return random.choice(list_entries())
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,34 @@
+from . import util
+
 from django.shortcuts import render
 
-from . import util
+from markdown2 import markdown
 
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+    context = {
+        "entries": util.list_entries(),
+        "random_entry": util.get_random_entry()
+    }
+    return render(request, "encyclopedia/index.html", context)
 
+
+def encyclopedia(request, title):
+    markdown_text = util.get_entry(title)
+
+    if markdown_text:
+        encyclopedia_html = markdown(markdown_text)
+        context = {
+            "title": title,
+            "encyclopedia_html": encyclopedia_html,
+            "random_entry": util.get_random_entry()
+        }
+        return render(request, "encyclopedia/encyclopedia.html", context)
+
+    context = {
+            "title": "Encyclopedia Not Found",
+            "error": f"Encyclopedia of {title} is not present.\
+                 Please feel free to add.",
+            "random_entry": util.get_random_entry()
+        }
+    return render(request, "encyclopedia/error.html", context)
